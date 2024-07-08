3bb1d954730051e54604b3c864f7876c0f52b10d
Francis Adeboye <adeboye.francis@icloud.com>
2024-07-08 18:56:20 +0100

    additional dependencies added to tomcat

--- src/main/java/com/example/demo_tomcat/DemoTomcatApplication.java
+++ src/main/java/com/example/demo_tomcat/DemoTomcatApplication.java
@@ -2,6 +2,7 @@ package com.example.demo_tomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoTomcatApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoTomcatApplication.class);
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTomcatApplication.class, args);
 	}
 
 	@GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "Deployment of maven project via Jenkins pipeline to Tomcat") String name) {
+public String hello(@RequestParam(value = "name", defaultValue = "Deployment of maven project via Jenkins pipeline to Tomcat") String name) {
     return String.format("%s", name);
 }
+
 
 }
